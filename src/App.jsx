685e27d39cafbb1de384c4a151b38bfe05c121c2
Grant Bellotti <55765767+Grant-Bellotti@users.noreply.github.com>
2025-02-09 20:49:57 -0800

    header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Welcome from './pages/Welcome'
+
+// import './reset.css';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <Welcome/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import ScrollingCarousel from "../components/ScrollingCarousel"
+import Navbar from "../components/Navbar/Navbar"
+
+const Welcome = () => {
+  return (
+    <div>
+        <Navbar/>
+        <ScrollingCarousel/>
+    </div>
+  )
+}
+
+export default Welcome

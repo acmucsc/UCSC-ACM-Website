d4d4b692848801cf91c2265daa9ccf7bc72326a1
rahp124 <punji.rahul@gmail.com>
2025-02-23 20:31:08 -0800

    fix

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 py-6 px-4 w-full fixed bottom-0">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+        {/* Left Menu */}
+        <div className="flex items-center">
+          {/* Blue Border (visible on all screens) */}
+          <div className="w-1 h-32 bg-blue-500 mr-6"></div>
+
+          {/* Menu Items */}
+          <div className="flex flex-col items-center md:items-start space-y-4">
+            <a
+              href="#home"
+              className="text-white hover:text-blue-400 text-lg no-underline"
+            >
+              Home
+            </a>
+            <a
+              href="#events"
+              className="text-white hover:text-blue-400 text-lg no-underline"
+            >
+              Events
+            </a>
+            <a
+              href="#about"
+              className="text-white hover:text-blue-400 text-lg no-underline"
+            >
+              About
+            </a>
+            <a
+              href="#team"
+              className="text-white hover:text-blue-400 text-lg no-underline"
+            >
+              Team
+            </a>
+          </div>
+        </div>
+
+        {/* Button */}
+        <div className="hidden md:flex justify-center md:justify-end">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold text-lg py-3 px-6 rounded-lg">
+            Join Mailing List
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,13 +1,18 @@
 import ScrollingCarousel from "../components/ScrollingCarousel"
 import Navbar from "../components/Navbar/Navbar"
+import Footer from "../components/Footer/Footer"
 
 const Welcome = () => {
   return (
-    <div>
-        <Navbar/>
-        <ScrollingCarousel/>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <ScrollingCarousel />
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
+
